f4cf3d94fed098b4c41be140cb96ec51efa76eed
Pedro-Jelev <pedroalves2801@gmail.com>
2025-01-31 11:39:53 -0300

    Root structure

--- src/main.tsx
+++ src/main.tsx
@@ -1,4 +1,15 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
+import { ThemeProvider } from "styled-components";
+import { GlobalStyles } from "./styles/global";
+import theme from "./styles/theme";
+import { Routes } from "./routes";
 
-createRoot(document.getElementById("root")!).render(<StrictMode></StrictMode>);
+createRoot(document.getElementById("root")!).render(
+  <StrictMode>
+    <ThemeProvider theme={theme}>
+      <GlobalStyles />
+      <Routes />
+    </ThemeProvider>
+  </StrictMode>
+);
